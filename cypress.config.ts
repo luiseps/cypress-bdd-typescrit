1d7ecf87a6b71aabdfb41d0959f18924f39bf3bb
Luis Perez <luiseps@MacBook-Pro-de-Luis.local>
2024-04-24 21:08:24 -0500

    tsconfig-2

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 import createBundler from "@bahmutov/cypress-esbuild-preprocessor";
 import { addCucumberPreprocessorPlugin , afterRunHandler} from "@badeball/cypress-cucumber-preprocessor";
 import createEsbuildPlugin from "@badeball/cypress-cucumber-preprocessor/esbuild";
-
+//const cypress_esbuild_preprocessor = require('@bahmutov/cypress-esbuild-preprocessor');
 const fs = require('fs').promises;
 const setupNodeEvents = async (on, config) => {
   await addCucumberPreprocessorPlugin(on, config, {
